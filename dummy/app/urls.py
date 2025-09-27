8fd52740aeff2e0667b09d5390368e88b4bfae92
João Serafim <victorserafim2003@gmail.com>
2025-09-27 11:51:01 -0300

    no-form html pages done

--- dummy/app/urls.py
+++ dummy/app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="Home Page")
+    path("", views.home, name="Home Page"),
+    path("about", views.about, name="About"),
+    path("start", views.start, name="Start"),
+    path("contact", views.contact, name="Contact")
 ]
--- dummy/app/views.py
+++ dummy/app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 def home(response):
     return render(response, 'home.html', {"logged": False})
+
+def about(response):
+    return render(response, 'about.html', {"logged": False})
+
+def start(response):
+    return render(response, 'start.html', {"logged": False})
+
+def contact(response):
+    return render(response, 'contact.html', {"logged": False})
